d5c41cf2366b408108ddf1f323f4b38dbbd83c27
maseology <mason@riffle.ca>
2019-08-02 15:47:35 -0400

    made Fibonacci consistent with interface

--- fibonacci.go
+++ fibonacci.go
@@ -10,13 +10,32 @@ const (
 //  UncertaintyLength = 0.01 'uncertainty length (l)
 //  DistinguishabilityConst = 0.01 'distinguishability constant (e)
 //  Sample range can be set to anything. Keeping consistent with other glbopt funcations, range hard coded to U[0.,1.]
-func Fibonacci(fun func(u float64) float64) (float64, float64) {
+//  This is only a 1-parameter optimizer, but need to keep slice variable input to maintain interface compatibility
+func Fibonacci(fun func(u1 []float64) float64) (float64, float64) {
 	// initilization step
 	const (
 		n  = 91 // largest Fibonacci number F(91) before overflow
 		a1 = 0. // range min
 		b1 = 1. // range max
 	)
+
+	// prime go functions
+	done := make(chan interface{})
+	defer close(done)
+	fun2 := func(done <-chan interface{}, v float64) <-chan float64 {
+		r := make(chan float64)
+		go func() {
+			defer close(r)
+			for {
+				select {
+				case r <- fun([]float64{v}):
+				case <-done:
+					return
+				}
+			}
+		}()
+		return r
+	}
 
 	// main step
 	ff1, ff2 := make([]float64, n+1), make([]float64, n+1)
@@ -25,8 +44,8 @@ func Fibonacci(fun func(u float64) float64) (float64, float64) {
 	bk[k] = b1
 	ff1[k] = fibonacciSample(ak[k], bk[k], float64(fibonacci(n-2)), float64(fibonacci(n))) // lambda
 	ff2[k] = fibonacciSample(ak[k], bk[k], float64(fibonacci(n-1)), float64(fibonacci(n))) // mu
-	ffr1 := fun(ff1[k])
-	ffr2 := fun(ff2[k])
+	ch1, ch2 := fun2(done, ff1[k]), fun2(done, ff2[k])
+	ffr1, ffr2 := <-ch1, <-ch2
 
 	for {
 		if ffr1 > ffr2 { // step 2
@@ -38,7 +57,7 @@ func Fibonacci(fun func(u float64) float64) (float64, float64) {
 				break
 			}
 			ffr1 = ffr2
-			ffr2 = fun(ff2[k+1])
+			ffr2 = fun([]float64{ff2[k+1]})
 		} else { // step 3
 			bk[k+1] = ff2[k]
 			ak[k+1] = ak[k]
@@ -48,9 +67,10 @@ func Fibonacci(fun func(u float64) float64) (float64, float64) {
 				break
 			}
 			ffr2 = ffr1
-			ffr1 = fun(ff1[k+1])
+			ffr1 = fun([]float64{ff1[k+1]})
 		}
 		k++ // step 4
+		print(".")
 	}
 
 	// step 5
@@ -59,8 +79,8 @@ func Fibonacci(fun func(u float64) float64) (float64, float64) {
 	if ff2[n] > b1 {
 		ff2[n] = b1
 	}
-	ffr1 = fun(ff1[n])
-	ffr2 = fun(ff2[n])
+	ffr1 = fun([]float64{ff1[n]})
+	ffr2 = fun([]float64{ff2[n]})
 	if ffr1 > ffr2 {
 		ak[n] = ff1[n]
 		bk[n] = bk[n-1]
@@ -70,8 +90,8 @@ func Fibonacci(fun func(u float64) float64) (float64, float64) {
 	}
 
 	// the optimum solution lies in the interval [ak(n), bk(n)], thus take average value
-	uopt := 0.5 * (ak[n] + bk[n])
-	return uopt, fun(uopt)
+	uopt := []float64{0.5 * (ak[n] + bk[n])}
+	return uopt[0], fun(uopt)
 }
 
 func fibonacciSample(a, b, fibonacciNumer, fibonacciDenom float64) float64 {
