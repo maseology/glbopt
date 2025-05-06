034a6bbc7c10acde1f30d589da8798c084e20acf
maseology <mason@riffle.ca>
2025-05-05 20:15:35 -0400

    updated messaging

--- dds.go
+++ dds.go
@@ -15,24 +15,25 @@ func DDS(nDim, nSmpl int, rng *rand.Rand, fun func(u []float64) float64, minimiz
 	// step2: random selection of initial samples
 	ibest := -1
 	ubest := make([]float64, nDim)
-	for d := 0; d < nDim; d++ {
+	for d := range nDim {
 		ubest[d] = rng.Float64()
 	}
 	fbest := fun(ubest)
 	fmt.Printf("  > initial    OF: %.6f  %.3f\n", fbest, ubest)
 
 	ldnm := math.Log(float64(nSmpl))
-	for i := 0; i < nSmpl; i++ {
+	for i := range nSmpl {
 		// step 3: random select
 		pi := 1 - math.Log(float64(i))/ldnm
 		var nn []int
-		for d := 0; d < nDim; d++ {
+		for d := range nDim {
 			if rng.Float64() > pi {
 				nn = append(nn, d)
 			}
 		}
 		if len(nn) == 0 {
 			nn = []int{rng.Intn(nDim)}
+			pi *= -1 // only to indicate that no values were chosen to be perturbed, only 1 value
 		}
 		// step 4: perturb
 		unew := make([]float64, nDim)
@@ -59,14 +60,14 @@ func DDS(nDim, nSmpl int, rng *rand.Rand, fun func(u []float64) float64, minimiz
 				fbest = fnew
 				ibest = i
 				copy(ubest, unew)
-				fmt.Printf("   iter %5d (%4f) OF: %.6f  %.3f\n", i, pi, fbest, ubest)
+				fmt.Printf("   iter %5d (%.4f) OF: %.6f  %.3f\n", i, pi, fbest, ubest)
 			}
 		} else {
 			if fnew >= fbest {
 				fbest = fnew
 				ibest = i
 				copy(ubest, unew)
-				fmt.Printf("   iter %5d (%4f) OF: %.6f  %.3f\n", i, pi, fbest, ubest)
+				fmt.Printf("   iter %5d (%.4f) OF: %.6f  %.3f\n", i, pi, fbest, ubest)
 			}
 		}
 	}
